3762f4ea7e39edd8a318dbff5df95b49aab8b847
FengNiMa <z4kong@eng.ucsd.edu>
2019-08-25 14:23:55 -0700

    update

--- run.sh
+++ run.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
+LR=0.001
+NEPOCH=1000
+
 python3 3_layer_ReLU_net_jupyter.py --dataset 'synthetic'\
   --params '100'\
-  --n_epoch 1000\
-  --lr 0.0002 &
+  --n_epoch $NEPOCH\
+  --lr $LR &
 python3 3_layer_ReLU_net_jupyter.py --dataset 'synthetic'\
-  --params '200'\
-  --n_epoch 1000\
-  --lr 0.0002 &
+  --params '50'\
+  --n_epoch $NEPOCH\
+  --lr $LR &
 python3 3_layer_ReLU_net_jupyter.py --dataset 'MNIST'\
   --params '01'\
-  --n_epoch 1000\
-  --lr 0.0002 &
+  --n_epoch $NEPOCH\
+  --lr $LR &
 python3 3_layer_ReLU_net_jupyter.py --dataset 'MNIST'\
   --params '17'\
-  --n_epoch 1000\
-  --lr 0.0002 &
+  --n_epoch $NEPOCH\
+  --lr $LR &
 python3 3_layer_ReLU_net_jupyter.py --dataset 'Fashion'\
   --params '01'\
-  --n_epoch 1000\
-  --lr 0.0002 &
+  --n_epoch $NEPOCH\
+  --lr $LR &
 python3 3_layer_ReLU_net_jupyter.py --dataset 'Fashion'\
   --params '79'\
-  --n_epoch 1000\
-  --lr 0.0002
-
+  --n_epoch $NEPOCH\
+  --lr $LR &
